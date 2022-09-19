c18b264132cf30fb9341a0b0399b34ea4bd63e83
nirupamaui97 <113470814+nirupamaui97@users.noreply.github.com>
2022-09-20 01:21:59 +0530

    Update app.component.ts

--- apps/angular-anim/src/app/app.component.ts
+++ apps/angular-anim/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { SideNavItem } from '@angular-anim/shared/presentational';
 import { UserFacade } from '@angular-anim/shared/store';
 import { Component, OnInit } from '@angular/core';
+import { Account } from 'libs/shared/services/src/lib/account';
+import { AccountService } from 'libs/shared/services/src/lib/account.service';
 import { startWith } from 'rxjs';
 
 @Component({
@@ -9,19 +11,31 @@ import { startWith } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  sideNavItems: SideNavItem[] = [];
+  sideNavItems: SideNavItem[] =  [];
+  userType = 'Not Logged In';
+  accounts: Account[] = [];
+  accountsFilter = '';
+
+  constructor(private userFacade: UserFacade, private accountService: AccountService) {
+   this.sideNavItems = [
+    { title: 'Account Overview', subtitle: '', link: '/' },
+    { title: 'Transfers', subtitle: '', link: '/transfers' },
+    { title: 'About Challenge', subtitle: '', link: '/about' },
+  ]; 
+}
+
+  ngOnInit(): void {
+    this.accountService.getAccounts().subscribe((accounts) => {
+      this.accounts = accounts;
+    });
+  }
+
+  filterAccounts(accounts: Account[]) {
+    return accounts.filter(acc => acc.currency === this.accountsFilter || this.accountsFilter === '');
+  }
+
   user$ = this.userFacade.getUser();
   userName$ = this.userFacade.getUserName().pipe(
-    startWith('Not Logged In'),
+    startWith(this.userType),
   )
-
-  constructor(private userFacade: UserFacade) { }
-
-  ngOnInit() {
-    this.sideNavItems = [
-      { title: 'Account Overview', subtitle: '', link: '/' },
-      { title: 'Transfers', subtitle: '', link: '/transfers' },
-      { title: 'About Challenge', subtitle: '', link: '/about' },
-    ]
-  }
 }
