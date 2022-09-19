482ff51eab8a843ec6e1feecf5e58b99974fa290
nirupamaui97 <113470814+nirupamaui97@users.noreply.github.com>
2022-09-20 01:23:38 +0530

    Update app-routing.module.ts

--- apps/angular-anim/src/app/app-routing.module.ts
+++ apps/angular-anim/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
+import { AccountRoutingDetailsComponent } from './account-routing-details/account-routing-details.component';
 
-// TODO-completed: 2. We've setup these routes and have them on the page but they aren't working
+// TODO: 2. We've setup these routes and have them on the page but they aren't working
 const routes: Routes = [
-  { path: '', component: AccountSummaryComponent },
-  { path: 'about', component: AboutComponent }
+  { path: '', redirectTo: '/account', pathMatch: 'full' },
+  { path: 'transfers', component: AccountSummaryComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'account', component: AccountDetailsComponent },
+  { path: 'accountDetails/:id', component: AccountRoutingDetailsComponent}
 ];
 
 @NgModule({
